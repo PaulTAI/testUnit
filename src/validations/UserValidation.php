baf8e1d9b2b108270b2a672308c75198b433ff84
Theo Faure <theo.faure77@hotmail.com>
2020-04-19 17:32:14 +0200

    v1

--- src/validations/UserValidation.php
+++ src/validations/UserValidation.php
@@ -3,23 +3,65 @@
 namespace App\validations;
 
 use App\Entity\User;
-use Doctrine\Persistence\ObjectManager;
 
 class UserValidation
 {
-    private $objectManager;
 
-    public function __construct(ObjectManager $objectManager)
+    public function __construct()
     {
-        $this->objectManager = $objectManager;
     }
 
-    public function isValid()
+    public function isValid(string $email, string $firstname, string $lastname, string $password, int $age, string $toDoListName)
     {
-        $userRepo = $this->objectManager
-            ->getRepository(User::class);
-        $user = $userRepo->find($id);
+        $errors = array(
+            "email" => array(),
+            "firstname" => array(),
+            "lastname" => array(),
+            "password" => array(),
+            "age" => array()
+        );
 
-        return ;
+        // email
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors["email"][0] = "format invalide";
+        };
+        if ($email != null) {
+            $errors["email"][1] = "vide";
+        };
+
+        // firstname
+        if ($firstname != null) {
+            $errors["firstname"][0] = "vide";
+        };
+        //lastname
+        if ($lastname != null) {
+            $errors["lastname"][0] = "vide";
+        };
+        //password
+        if($password != null) {
+            $errors["password"][0] = "vide";
+        }
+        if(strlen($password) < 8 || strlen($password) > 40) {
+            $errors["password"][1] = "mauvaise taille";
+        }
+        //age 
+        if($age < 13) {
+            $errors["age"][0] = "age invalide";
+        }
+
+        if(
+            $errors["email"][0] == null && 
+            $errors["email"][1] == null && 
+            $errors["firstname"][0] == null && 
+            $errors["lastname"][0] == null && 
+            $errors["password"][0] == null && 
+            $errors["password"][1] == null && 
+            $errors["age"][0] == null && 
+            $errors["age"][1] == null
+        ) {
+            return true;
+        }else {
+            return false;
+        }
     }
 }
