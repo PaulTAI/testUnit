b91f24e43d51d03eec86d89d086e099c3ba2ef56
Kelshyr <paul.taitinger@open-groupe.com>
2020-04-19 15:50:56 +0200

    entities

---
+++ src/Entity/Items.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ItemsRepository")
+ */
+class Items
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\todolist", inversedBy="items")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $createDate;
+
+    public function __construct()
+    {
+        $this->name = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Collection|todolist[]
+     */
+    public function getName(): Collection
+    {
+        return $this->name;
+    }
+
+    public function addName(todolist $name): self
+    {
+        if (!$this->name->contains($name)) {
+            $this->name[] = $name;
+        }
+
+        return $this;
+    }
+
+    public function removeName(todolist $name): self
+    {
+        if ($this->name->contains($name)) {
+            $this->name->removeElement($name);
+        }
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreateDate(): ?\DateTimeInterface
+    {
+        return $this->createDate;
+    }
+
+    public function setCreateDate(\DateTimeInterface $createDate): self
+    {
+        $this->createDate = $createDate;
+
+        return $this;
+    }
+}
---
+++ src/Entity/ToDoList.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ToDoListRepository")
+ */
+class ToDoList
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\user", mappedBy="ToDoList")
+     */
+    private $name;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Items", mappedBy="name")
+     */
+    private $items;
+
+    public function __construct()
+    {
+        $this->name = new ArrayCollection();
+        $this->items = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Collection|user[]
+     */
+    public function getName(): Collection
+    {
+        return $this->name;
+    }
+
+    public function addName(user $name): self
+    {
+        if (!$this->name->contains($name)) {
+            $this->name[] = $name;
+            $name->setToDoList($this);
+        }
+
+        return $this;
+    }
+
+    public function removeName(user $name): self
+    {
+        if ($this->name->contains($name)) {
+            $this->name->removeElement($name);
+            // set the owning side to null (unless already changed)
+            if ($name->getToDoList() === $this) {
+                $name->setToDoList(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Items[]
+     */
+    public function getItems(): Collection
+    {
+        return $this->items;
+    }
+
+    public function addItem(Items $item): self
+    {
+        if (!$this->items->contains($item)) {
+            $this->items[] = $item;
+            $item->addName($this);
+        }
+
+        return $this;
+    }
+
+    public function removeItem(Items $item): self
+    {
+        if ($this->items->contains($item)) {
+            $this->items->removeElement($item);
+            $item->removeName($this);
+        }
+
+        return $this;
+    }
+}
